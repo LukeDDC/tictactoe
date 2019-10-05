7a9097f91b9537e960c5e9b4cacb1d7aec41d802
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-10-05 20:26:56 -0300

    feat(MainWindow): Creating 3x3 button matrix in the mainWindow constructor.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "gamebutton.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int row = 0; row< 3; ++row) {
+        for (int column = 0; column < 3; ++column) {
+            GameButton *button = new GameButton(row, column, ui->gridLayoutWidget);
+            button->setText("");
+            ui->gridLayout->addWidget(button, row, column);
+        }
+    }
 }
 
 MainWindow::~MainWindow()
